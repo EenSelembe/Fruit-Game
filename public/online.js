1833949042db6a25451330bf70d49779e0c52f94
EenSelembe <rendy.waniko@gmail.com>
2025-09-26 23:39:40 +0700

    Update online.js

--- public/online.js
+++ public/online.js
@@ -1,8 +1,21 @@
+// online.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 import { getFirestore, doc, updateDoc } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-const auth = getAuth();
-const db = getFirestore();
+const firebaseConfig = {
+  apiKey: "AIzaSyB8g9X_En_sJnbdT_Rc1NK88dUdbg3y2nE",
+  authDomain: "fruit-game-5e4a8.firebaseapp.com",
+  projectId: "fruit-game-5e4a8",
+  storageBucket: "fruit-game-5e4a8.appspot.com",
+  messagingSenderId: "936228678997",
+  appId: "1:936228678997:web:9dab2fa0d9a019161bd3dc",
+  measurementId: "G-EPTSQQPM4D"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 export function initOnlineStatus() {
   onAuthStateChanged(auth, async (user) => {
@@ -15,6 +28,15 @@ export function initOnlineStatus() {
       }
     } else {
       window.location.href = "index.html";
+    }
+  });
+
+  // Saat tab ditutup → offline
+  window.addEventListener("beforeunload", () => {
+    const user = auth.currentUser;
+    if (user) {
+      const meRef = doc(db, "users", user.uid);
+      updateDoc(meRef, { isOnline: false });
     }
   });
 }
