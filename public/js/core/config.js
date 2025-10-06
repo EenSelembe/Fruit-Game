5a099bc387aca9f5ed9bfd2b0395e1f4fd8ecc3a
EenSelembe <rendy.waniko@gmail.com>
2025-10-06 15:40:27 +0700

    Create config.js

---
+++ public/js/core/config.js
@@ -0,0 +1,5 @@
+// public/js/core/config.js
+export const RAINBOW = ["#ff0055","#ff7b00","#ffee00","#00d26a","#00b3ff","#6950ff"];
+export const DEFAULT_PLAYER_COLOR = "#58ff9b";
+export const WORLD_DEFAULT = { w: 4200, h: 4200, grid: 90 };
+export const FOOD_COUNT_DEFAULT = 1400;
