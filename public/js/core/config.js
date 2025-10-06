529b011359909acc4e84e0d130eb9f48f854475b
EenSelembe <rendy.waniko@gmail.com>
2025-10-07 00:27:02 +0700

    Update config.js

--- public/js/core/config.js
+++ public/js/core/config.js
@@ -3,13 +3,3 @@ export const RAINBOW = ["#ff0055","#ff7b00","#ffee00","#00d26a","#00b3ff","#6950
 export const DEFAULT_PLAYER_COLOR = "#58ff9b";
 export const WORLD_DEFAULT = { w: 4200, h: 4200, grid: 90 };
 export const FOOD_COUNT_DEFAULT = 1400;
-
-export const BOT_PALETTES = [
-  ["#79a7ff","#79ffd1"],
-  ["#ff7b00","#ffee00"],
-  ["#ff4d6d","#a06cff"],
-  ["#00d26a","#00b3ff"],
-  ["#ff0055","#6950ff"],
-  ["#ffee00","#00d26a","#00b3ff"],
-  ["#ff8a00","#ff2d55","#8e44ad"]
-];
